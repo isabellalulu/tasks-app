63436d2295fa3e69f6cd7a74ca0a8ff57aede6e6
isabellalulu <eubellalulu@gmail.com>
2026-02-04 00:58:23 -0300

    adiciona modo inicial

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 280,
+    height: 360,
+    resizable: false,
+    maximizable: false,
+    fullscreenable: false,
+    frame: false, 
+    transparent: false,
+    webPreferences: {
+        nodeIntegration: true, 
+        contextIsolation: false
+    }
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+  });
+  
+  ipcMain.on('close-app', () => {
+      app.quit();
+    });
---
+++ script.js
@@ -0,0 +1,24 @@
+// Se você mudou o main.js como sugeri acima, isso vai funcionar:
+const { ipcRenderer } = require('electron');
+
+function updateCalendar(){
+    const now = new Date();
+    const day = now.getDate();
+    const monthNames = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", 
+    "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"];
+
+    const month = monthNames[now.getMonth()];
+
+    // Usando textContent para garantir a exibição
+    document.getElementById('day').textContent = day;
+    document.getElementById('month').textContent = month;
+}
+
+// Inicializa
+updateCalendar();
+
+// Botão fechar
+document.getElementById('close-btn').addEventListener('click', () => {
+    ipcRenderer.send('close-app');
+});
+
